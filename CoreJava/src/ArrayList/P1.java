9049bbadebcf4977f4751b10730deec32a1b42da
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2022-12-23 21:17:13 +0530

    Collection-ArrayList

---
+++ CoreJava/src/ArrayList/P1.java
@@ -0,0 +1,27 @@
+package ArrayList;
+
+import java.util.*;
+
+public class P1 {
+	
+	public static void main(String[] args) {
+		
+		ArrayList  list=new ArrayList();
+		
+		list.add("A");
+		list.add(10);
+		list.add("B");
+		list.add("C");
+		list.add("A");
+		list.add(null);
+		
+		System.out.println(list);
+	
+		list.remove(list.indexOf("A"));
+		list.set(2, "D");
+		System.out.println(list);
+		
+		
+	}
+
+}
