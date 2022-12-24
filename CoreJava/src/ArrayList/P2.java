08e7e7b0e9229dae4c36ca261badc92c098b2b16
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2022-12-24 12:11:06 +0530

    Linked List

---
+++ CoreJava/src/ArrayList/P2.java
@@ -0,0 +1,19 @@
+package ArrayList;
+
+import java.util.*;
+
+public class P2 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		ArrayList l=new ArrayList();
+		
+		
+		LinkedList ll=new LinkedList(l);
+		
+		
+	}
+
+}
