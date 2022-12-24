34257519de133e9b694a02f09b8f7536620d89ca
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2022-12-24 13:19:46 +0530

    Vector

---
+++ CoreJava/src/ArrayList/P4.java
@@ -0,0 +1,26 @@
+package ArrayList;
+
+import java.util.*;
+
+public class P4 {
+	
+	public static void main(String[] args) {
+		
+		Vector v=new Vector();
+		
+		System.out.println("Intial Capacity: "+v.capacity());
+		
+		for(int i=1;i<=10;i++) {
+			v.addElement(i);
+		}
+		
+		System.out.println(v);
+		
+		v.add("Mahesh");
+		System.out.println(v);
+		System.out.println(v.capacity());
+		
+		
+	}
+
+}
