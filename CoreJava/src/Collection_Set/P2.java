8b9e2cfa6ca9835fd50c19fcf45d312473a85c51
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2022-12-27 19:11:33 +0530

    Linked Hash Set

---
+++ CoreJava/src/Collection_Set/P2.java
@@ -0,0 +1,26 @@
+package Collection_Set;
+
+import java.util.*;
+
+public class P2 {
+	
+	
+	public static void main(String[] args) {
+		
+		LinkedHashSet lhs=new LinkedHashSet();
+		
+		lhs.add("A");
+		lhs.add("B");
+		lhs.add(null);
+		lhs.add(3);
+		lhs.add(6);
+		System.out.println(lhs.add("B"));
+		System.out.println(lhs);
+		
+	}
+	
+	
+	
+	
+
+}
