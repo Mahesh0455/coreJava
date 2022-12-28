536afeef4107244d9bc56897028efa4ff182f879
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2022-12-28 20:06:09 +0530

    Linked Hash Map

---
+++ CoreJava/src/Collection_Map/P2.java
@@ -0,0 +1,24 @@
+package Collection_Map;
+
+import java.util.*;
+
+public class P2 {
+	
+	public static void main(String[] args) {
+		
+		LinkedHashMap m=new LinkedHashMap();
+		m.put("Neel", 18500);
+		m.put("Mahesh", 15000);
+		m.put("Pawar", 12000);
+		m.put("Sagar", 16000);
+		m.put("Keshav", 18000);
+		
+		System.out.println(m);
+		
+		//Insertion order is preserved in LinkedHash Map
+		
+	}
+	
+	
+
+}
