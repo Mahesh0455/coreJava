96a9e10bbac841d5397423131e8a977a4c67f006
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2022-12-28 10:55:48 +0530

    Adding Heterogenous Objects in Tree set

---
+++ CoreJava/src/Collection_Set/P9.java
@@ -0,0 +1,45 @@
+package Collection_Set;
+
+import java.util.*;
+
+public class P9 {
+
+	public static void main(String[] args) {
+
+		TreeSet t = new TreeSet(new MyComparator1());
+
+		t.add("A");
+		t.add(new StringBuffer("ABC"));
+		t.add(new StringBuffer("AA"));
+		t.add("xx");
+		t.add("ABCD");
+		t.add("A");
+		
+		System.out.println(t);
+
+	}
+
+}
+
+class MyComparator1 implements Comparator {
+
+	@Override
+	public int compare(Object o1, Object o2) {
+
+		String s1 = o1.toString();
+		String s2 = o2.toString();
+
+		int l1 = s1.length();
+		int l2 = s2.length();
+
+		if (l1 > l2) {
+			return 1;
+		} else if (l1 < l2) {
+			return -1;
+		} else {
+			return s1.compareTo(s2);
+		}
+
+	}
+
+}
