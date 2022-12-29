39bb73a4979a96faecff2dd9e98bf6ca8657d6d3
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2022-12-29 10:33:16 +0530

    Tree Map

--- CoreJava/src/Collection_Map/P5.java
+++ CoreJava/src/Collection_Map/P5.java
@@ -1,5 +1,25 @@
 package Collection_Map;
+import java.util.*;
 
 public class P5 {
+	
+	
+	public static void main(String[] args) {
+		
+		IdentityHashMap m=new IdentityHashMap();
+		
+		String s1=new String ("Mahesh");
+		String s2=new String("Mahesh");
+		
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s2));
+		
+		m.put(s1, "hello");
+		m.put(s2, "Hi");
+		
+		System.out.println(m);
+	}
+	
+	
 
 }
---
+++ CoreJava/src/Collection_Map/P6.java
@@ -0,0 +1,19 @@
+package Collection_Map;
+import java.util.*;
+
+public class P6 {
+	
+	public static void main(String[] args) {
+		
+		
+		TreeMap t=new TreeMap();
+		
+		t.put(100, "ZZZ");
+		t.put(103, "YYY");
+		t.put(101, "XXX");
+		t.put(104, 106);
+		
+		System.out.println(t);
+	}
+
+}
