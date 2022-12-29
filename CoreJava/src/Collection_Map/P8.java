bf70a38a64505cb08a734e294c8895db24bd3cb1
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2022-12-29 19:28:47 +0530

    Hashtable

---
+++ CoreJava/src/Collection_Map/P8.java
@@ -0,0 +1,44 @@
+package Collection_Map;
+import java.util.*;
+
+public class P8 {
+	
+	public static void main(String[] args) {
+		
+		Hashtable h=new Hashtable();
+		h.put(new Temp(5), "A");
+		h.put(new Temp(2), "B");
+		h.put(new Temp(6), "C");
+		h.put(new Temp(15), "D");
+		h.put(new Temp(23), "E");
+		h.put(new Temp(16), "F");
+		h.put(new Temp(5), "AA");
+	
+		System.out.println(h);
+		
+		
+		
+	}
+
+}
+
+
+class Temp{
+	
+	int i;
+	
+	Temp(int i){
+		this.i=i;
+		
+	}
+	
+	public int hashCode() {
+		
+		return i;
+	}
+	
+	public String toString() {
+		
+		return i+"";
+	}
+}
