0e5dc89e0ba0b25fa874cef15317e8f1a4c6e29c
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2022-12-31 10:41:09 +0530

    Collections binary search

---
+++ CoreJava/src/Collection_List/P7.java
@@ -0,0 +1,30 @@
+package Collection_List;
+
+import java.util.*;
+
+public class P7 {
+	
+	public static void main(String[] args) {
+		
+		ArrayList l=new ArrayList();
+		l.add("Z");
+		l.add("A");
+		l.add("M");
+		l.add("K");
+		l.add("a");
+		
+		System.out.println("Before sorting"+l);
+		
+		Collections.sort(l);
+		
+		System.out.println("After Sorting"+l);
+		
+		System.out.println(Collections.binarySearch(l, "M"));
+		
+		System.out.println(Collections.binarySearch(l, "J"));
+		
+		
+		
+	}
+
+}
