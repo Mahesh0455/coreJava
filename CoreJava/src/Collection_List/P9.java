5a46c5c5cbdb140c1478614ea26b8c5b308eb597
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2023-01-07 11:34:29 +0530

    comment

--- CoreJava/src/Collection_List/P9.java
+++ CoreJava/src/Collection_List/P9.java
@@ -7,12 +7,16 @@ public class P9 {
 	public static void main(String[] args) {
 		
 		ArrayList list=new ArrayList();
+		Employee e1=new Employee(101,"Raj");
+		Employee e2=new Employee(102,"Ram");
 		
 		list.add(20);
 		list.add(30);
 		list.add(40);
 		list.add(50);
 		list.add("Mahesh");
+		list.add(e1);
+		list.add(e2);
 		
 		System.out.println(list.size());
 		
@@ -52,5 +56,9 @@ class Employee{
 	public String getEmpName(){
 		return this.empName;
 		
+	}
+	
+	public String toString() {
+		return "Employee["+empId+"="+empName+"]";
 	}
 }
