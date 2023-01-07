d0262f6b6e79763e3608e7f16c0c812a19361959
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2023-01-07 11:25:19 +0530

    User-defined class

--- CoreJava/src/Collection_List/P9.java
+++ CoreJava/src/Collection_List/P9.java
@@ -25,3 +25,14 @@ public class P9 {
 	}
 
 }
+
+class Employee{
+	
+	int empId;
+	String empName;
+	
+	public Employee(int empId,String empName) {
+		this.empId=empId;
+		this.empName=empName;
+	}
+}
