a3c4a11b6d466ff9e1edf01b55d0f33f49b6609a
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2023-01-14 14:25:06 +0530

    generic 3

--- CoreJava/src/Collection_List/P13.java
+++ CoreJava/src/Collection_List/P13.java
@@ -8,7 +8,7 @@ public class P13 {
 		
 		ArrayList<String> l=new ArrayList();
 		l.add("M");
-		l.add(10);
+		//l.add(10);
 		
 		System.out.println(l);
 		
---
+++ CoreJava/src/Collection_List/P14.java
@@ -0,0 +1,15 @@
+package Collection_List;
+
+import java.util.ArrayList;
+
+public class P14 {
+	
+	public static void main(String[] args) {
+		ArrayList<String> l=new ArrayList<String>();
+		l.add("Mp");
+		/*
+		 * l.add(new Object()); l.add(10);
+		 */
+	}
+
+}
