a8c94d62181dda25e605f38da6d4fed4c1b54b33
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2023-01-14 10:49:36 +0530

    Utility methods

---
+++ CoreJava/src/Collection_List/P11.java
@@ -0,0 +1,20 @@
+package Collection_List;
+import java.util.*;
+
+public class P11 {
+	
+	public static void main(String[] args) {
+		
+		ArrayList al=new ArrayList();
+		
+		al.add("M");
+		al.add("P");
+		al.add(1);
+		
+		System.out.println(al);
+		
+		//Collections.sort(al);
+		
+	}
+
+}
