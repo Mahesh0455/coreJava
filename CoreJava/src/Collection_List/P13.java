2c6b3372b073d3baea99a996ca87b2449c87d4da
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2023-01-14 14:23:24 +0530

    generic2

---
+++ CoreJava/src/Collection_List/P13.java
@@ -0,0 +1,16 @@
+package Collection_List;
+
+import java.util.ArrayList;
+
+public class P13 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> l=new ArrayList();
+		l.add("M");
+		l.add(10);
+		
+		System.out.println(l);
+		
+	}
+}
