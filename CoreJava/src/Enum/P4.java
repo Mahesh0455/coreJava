3edf8935b3df9d9d82e1f882bfa33923f2efbc02
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2023-01-14 19:46:48 +0530

    enum4

---
+++ CoreJava/src/Enum/P4.java
@@ -0,0 +1,21 @@
+package Enum;
+
+public class P4 {
+	
+	public static void main(String[] args) {
+		
+		Beer b[]=Beer.values();
+		
+		for(Beer b1:b) {
+			System.out.println(b1);
+		}
+		
+	}
+
+}
+
+
+enum Beer{
+	
+	KF,KO,RC,FO;
+}
