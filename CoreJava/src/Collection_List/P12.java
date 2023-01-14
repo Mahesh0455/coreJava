3802f96a87fc9cf9272b64d1d96a2e6ad73f6e55
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2023-01-14 14:21:08 +0530

    generic

---
+++ CoreJava/src/Collection_List/P12.java
@@ -0,0 +1,18 @@
+package Collection_List;
+
+import java.util.ArrayList;
+
+public class P12 {
+	
+	public static void main(String[] args) {
+		
+		ArrayList l=new ArrayList<String>();
+		l.add("A");
+		l.add(10);
+		
+		System.out.println(l);
+	
+		
+	}
+
+}
