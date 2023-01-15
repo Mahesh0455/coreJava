962039fa025192cbb59df89cc5c5793dee66a14c
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2023-01-15 13:45:39 +0530

    Development

---
+++ CoreJava/src/Development/P1.java
@@ -0,0 +1,15 @@
+package Development;
+
+import java.util.Properties;
+
+public class P1 {
+	
+	public static void main(String[] args) {
+		
+		
+		Properties p=System.getProperties();
+		
+		p.list(System.out);
+	}
+
+}
