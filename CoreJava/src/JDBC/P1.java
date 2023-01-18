792193eb25e18866ec793ea4fbd90f2782e602b9
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2023-01-18 22:28:37 +0530

    jdbc-1

---
+++ CoreJava/src/JDBC/P1.java
@@ -0,0 +1,36 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class P1 {
+		
+		public static void main(String[] args) throws SQLException, ClassNotFoundException {
+			  
+			
+			Connection con;
+			
+			con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/dvdrental",
+		            "postgres", "root");
+			
+			Statement st=con.createStatement();
+			ResultSet rs=st.executeQuery("select * from actor");
+			
+			while(rs.next()) {
+				
+				System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+				
+			}
+			
+			
+			
+			
+			
+			
+			
+		}
+
+}
