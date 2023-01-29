569371c77a0ad45a21c490fab4717303c9af6019
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2023-01-29 11:00:57 +0530

    Singleton class

---
+++ CoreJava/src/OOPS/P11.java
@@ -0,0 +1,37 @@
+package OOPS;
+
+public class P11 {
+
+	
+	public static void main(String[] args) {
+	
+		System.out.println(Singleton.getSingleton().hashCode());
+		System.out.println(Singleton.getSingleton().hashCode());
+		System.out.println(Singleton.getSingleton().hashCode());
+		System.out.println(Singleton.getSingleton().hashCode());
+
+		
+	}
+	
+}
+
+
+class Singleton{
+	
+	
+	
+	private static Singleton s=null;
+	
+	private Singleton() {};
+	
+	public static Singleton getSingleton() {
+		
+		if(s==null) {
+			s=new Singleton();
+		}
+		
+		return s;
+	}
+	
+	
+}
