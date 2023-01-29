2361e810c4ccbb19de2a251dad01f694a47b8096
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2023-01-29 13:01:08 +0530

    type casting

---
+++ CoreJava/src/OOPS/P12.java
@@ -0,0 +1,22 @@
+package OOPS;
+
+public class P12 {
+	
+	public static void main(String[] args) {
+		
+		Object o=new String("Mahesh");
+		
+		
+		
+		
+		String s=(String)o;
+		
+		
+		
+		
+		
+	}
+
+}
+
+
